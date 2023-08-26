a60ff7e00a5c3b1d1697dc18376a27842ad8036b
coffeholic404 <taqealibade@gmail.com>
2023-08-26 22:04:27 +0300

    adding navigation component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,33 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: [
+    'react-refresh',
+    "react", 
+    "import", 
+    "jsx-a11y",
+],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0
+  },
+  "import/resolver": {
+    "node": {
+      "extensions": [".js", ".jsx"]
+    }
   },
 }
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './routes/home/Home';
+
+import Navigation from './routes/navigation/navigation';
+
+function App() {
+  return (
+
+    <Routes>
+
+      <Route path='/' element={<Navigation />}>
+      <Route index element={<Home />}/>
+
+      </Route>
+      
+    </Routes>
+  )
+}
+
+export default App;
